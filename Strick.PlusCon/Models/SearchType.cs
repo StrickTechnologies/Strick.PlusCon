c52ac73985ab85aa456545a5ef0209fc629e4d6f
Jim Strickland <79662634+JimStrickland@users.noreply.github.com>
2024-01-09 10:50:08 -0500

    XML doc updates

--- Strick.PlusCon/Models/SearchType.cs
+++ Strick.PlusCon/Models/SearchType.cs
@@ -1,10 +1,28 @@
 ﻿namespace Strick.PlusCon.Models;
 
 
+/// <summary>
+/// Specifies the type of search to perform
+/// </summary>
 public enum SearchType
 {
+	/// <summary>
+	/// Indicates that a search should determine whether the search string matches a string anywhere in a field
+	/// </summary>
 	Contains = 0,
+
+	/// <summary>
+	/// Indicates that a search should determine whether the search string matches a string at the beginning of a field
+	/// </summary>
 	StartsWith = 1,
+
+	/// <summary>
+	/// Indicates that a search should determine whether the search string matches a string at the end of a field
+	/// </summary>
 	EndsWith = 2,
+
+	/// <summary>
+	/// Indicates that a search should determine whether the search string matches a string
+	/// </summary>
 	Equals = 3
 }
