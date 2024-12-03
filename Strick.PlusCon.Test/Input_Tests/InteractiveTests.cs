49e350eb283978ca6ccc1a52db34e28c81a5ea58
Jim Strickland <79662634+JimStrickland@users.noreply.github.com>
2024-12-03 07:55:59 -0500

    Widget refactor

--- Strick.PlusCon.Test/Input_Tests/InteractiveTests.cs
+++ Strick.PlusCon.Test/Input_Tests/InteractiveTests.cs
@@ -324,7 +324,7 @@ internal class InteractiveTests
 	{
 		WL();
 
-		List<Widget> choices = [Widget.SmallWidget, Widget.MediumWidget, Widget.LargeWidget];
+		List<Widget> choices = Widget.AllWidgets().ToList();
 
 		var args = new InputArgumentsSelect<Widget>("Choose a Widget: ", choices) { SelectionOptionStyle = new(Color.White, Color.Red) };
 		Widget? sel = Input.Select(args);
--- Strick.PlusCon.Test/Models/Widget.cs
+++ Strick.PlusCon.Test/Models/Widget.cs
@@ -9,9 +9,11 @@
 /// <param name="price"><inheritdoc cref="Price" path="/summary"/></param>
 internal class Widget(int id, string name, decimal price)
 {
-	public static Widget SmallWidget => new Widget(1001, "Small Widget", 1.25M);
-	public static Widget MediumWidget => new Widget(1002, "Medium Widget", 2.33M);
-	public static Widget LargeWidget => new Widget(1003, "Large Widget", 3.49M);
+	public static Widget SmallWidget() => new Widget(1001, "Small Widget", 1.25M);
+	public static Widget MediumWidget() => new Widget(1002, "Medium Widget", 2.33M);
+	public static Widget LargeWidget() => new Widget(1003, "Large Widget", 3.49M);
+
+	public static IEnumerable<Widget> AllWidgets() => [SmallWidget(), MediumWidget(), LargeWidget()];
 
 
 	public override string ToString() => $"{GetType().Name} {Id}-{Name} ({Price:C2})";
